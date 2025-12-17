062e7e504c454f62247a4c605583c8e2aa734125
Jeyoung Woo <dnwodud3@gmail.com>
2025-12-17 22:09:53 +0900

    MemberMapper.xml 코드 수정

--- AlternativeService/src/main/java/com/web/as/config/SecurityInitializer.java
+++ AlternativeService/src/main/java/com/web/as/config/SecurityInitializer.java
@@ -2,6 +2,7 @@ package com.web.as.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
-	 
+// Spring Security 필터를 자동 등록
+public class SecurityInitializer
+        extends AbstractSecurityWebApplicationInitializer {
 }
